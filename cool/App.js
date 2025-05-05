118233ea05cde384c64082b520275ec5873e63d0
David S <dshi2008mayo@gmail.com>
2025-05-04 21:54:39 -0400

    Add backend, frontend, planning, and cool folders

---
+++ cool/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>https://hacknight.co/</Text>
+      <Text>https://hacknight.co/</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
